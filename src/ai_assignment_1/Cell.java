9322bd87803055418d9a420bb3dea74959c87312
fagoand <fagoand@hotmail.com>
2014-01-20 18:24:26 +0100

    Adjustments after first meeting

--- src/ai_assignment_1/Cell.java
+++ src/ai_assignment_1/Cell.java
@@ -2,5 +2,5 @@ package ai_assignment_1;
 
 public class Cell {
 	public boolean state;//True for Dirty, False for clean
-	public String label;
+	public String label; 
 }
--- src/ai_assignment_1/VacuumAgent.java
+++ src/ai_assignment_1/VacuumAgent.java
@@ -4,13 +4,21 @@ import java.util.Vector;
 public class VacuumAgent {
 	private int score;
 	private int steps;
-	private Vector<Cell> memory;
+	public static int BAG_CAPACITY = 50;
+	
+	public class memCell extends Cell{
+		public boolean firstLeft;
+		public boolean lastRight;
+	};
+	
+	private Vector<memCell> memory;
 	
 	public VacuumAgent(){}
 	public void start(Environment environment){}
 	public void stop(){}
 	public void showPerformance(){}
 	public void checkEnvironmentCellState(){}
+	private void replaceBag(){}
 	private void suck(){}
 	private void noOp(){}
 	private void moveLeft(){}
